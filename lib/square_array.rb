ca16d9b8974971276ec83bc32209c6f80571acfa
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-05-10 19:37:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
+square_array = [1,2,3,4,5]
+new_array = []
+ 
+def square_array.length.times do |index|
+  new_array.push(array[index] + 1)
 end
+new_array
